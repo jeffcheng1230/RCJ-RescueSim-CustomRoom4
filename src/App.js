982bc6c1314121cf41a00b8c53e2e50ceaa76050
Jeffrey Cheng <jeffreycheng1230@gmail.com>
2023-01-11 00:36:30 -0500

    basic opencv, upload/download file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { OpenCvProvider, useOpenCv } from 'opencv-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var cv;
+
+function OpenCvElement() {
+  const data = useOpenCv()
+  cv = data.cv;
+  return <></>
 }
 
-export default App;
+function App() {
+
+  var imgRef = React.createRef();
+  var downloadRef = React.createRef();
+
+  async function createWorld(e) {
+    if (cv) {
+      var img = imgRef.current;
+      img.src = URL.createObjectURL(e.target.files[0]);
+      var mat = cv.imread(img);
+
+      const blob = new Blob(["hello"], { type: "txt" });
+      downloadRef.current.href = URL.createObjectURL(blob);
+      downloadRef.current.download = "test.txt";
+    }
+  }
+
+  return (
+    <OpenCvProvider openCvPath='/opencv/opencv.js'>
+      <OpenCvElement />
+      <h1>RCJ Rescue Simulation Custom Room 4 Creation</h1>
+      Select Room 4 Picture: <input type="file" id="fileInput" onChange={createWorld} name="file" />
+      <img ref={imgRef} src="logo192.png" style={{display: "none"}}></img>
+      <a ref={downloadRef}>Download</a>
+    </OpenCvProvider>
+  )
+}
+
+export default App
